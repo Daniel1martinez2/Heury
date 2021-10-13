f0fbbdef4adcf9569e9297a9c2157e79101349e7
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2021-10-13 18:07:57 -0500

    Empty message done ✨
    Signed-off-by: Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>

--- src/HeuristicChunk/Document/DocumentArea.tsx
+++ src/HeuristicChunk/Document/DocumentArea.tsx
@@ -2,14 +2,15 @@ import React, {useContext} from 'react'
 import DocumentView from './DocumentView'
 import styles from './DocumentArea.module.css';
 import ProjectContext from '../../store/project-context';
+import EmptyDocument from './EmptyDocument/EmptyDocument';
 
 const DocumentArea = () => {
   const ctx = useContext(ProjectContext); 
   const {observationArray} = ctx; 
   return (
     <div className={styles['area']}>
+      {observationArray.length === 0 && <EmptyDocument/>}
       {observationArray.map((observation, index) => <DocumentView observationData={observation} index={index}/>)}
-      
     </div>
   )
 }
