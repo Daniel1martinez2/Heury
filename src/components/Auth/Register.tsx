31b216cda7eab0972f88624893a88407296d4bba
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2021-12-21 23:06:16 -0500

    login and register components fixed
    Signed-off-by: Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>

--- src/components/Auth/Register.tsx
+++ src/components/Auth/Register.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react';
 import user from '../../library/img/user.svg'; 
 import mail from '../../library/img/mail.svg'; 
 import lock from '../../library/img/lock.svg'; 
@@ -7,11 +7,37 @@ import {motion} from 'framer-motion';
 import {appear} from '../../library/common/commonData'; 
 import AuthInput from '../AuthInput/AuthInput'; 
 
+
+
+
+
+
+
+
+
 interface RegisterInterface {
   setMode: React.Dispatch<React.SetStateAction<string>>; 
 }
 
 const Register:React.FC<RegisterInterface> = ({setMode}) => {
+  
+  
+  const [name, setName] = useState('');
+  const [mailInput, setMailInput] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmedPassword, setConfirmedPassword] = useState('');
+
+  const setNameHandler: React.ChangeEventHandler<HTMLInputElement> = (e) => setName(e.target.value); 
+  const mailInputHandler: React.ChangeEventHandler<HTMLInputElement> = (e) => setMailInput(e.target.value); 
+  const passwordHandler: React.ChangeEventHandler<HTMLInputElement> = (e) => setPassword(e.target.value); 
+  const confirmedPasswordHandler: React.ChangeEventHandler<HTMLInputElement> = (e) => setConfirmedPassword(e.target.value); 
+
+  const submitHandler:React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault(); 
+    console.log(name, mailInput, password, confirmedPassword);
+    
+  }
+
   return (
     <motion.form 
       className={styles['form']}
@@ -19,15 +45,48 @@ const Register:React.FC<RegisterInterface> = ({setMode}) => {
       initial="hidden"
       animate="visible"
       exit="exit"
+      onSubmit={submitHandler}
     >
-
-      <AuthInput label="Name" placeholder="name" icon={user} type={'text'} />
-      <AuthInput label="Mail" placeholder="user@mail.com" icon={mail} type={'text'} />
-      <AuthInput label="Password" placeholder="6 characters" icon={lock} type={'password'} />
-      <AuthInput label="Confirm Password" placeholder="6 characters" icon={lock} type={'password'} />
+      <AuthInput 
+        label="Name" 
+        placeholder="name" 
+        icon={user} 
+        type={'text'} 
+        defaultText={name}
+        handleChange={setNameHandler}
+      />
+      <AuthInput 
+        label="Mail" 
+        placeholder="user@mail.com" 
+        icon={mail} 
+        type={'text'}
+        defaultText={mailInput}
+        handleChange={mailInputHandler}
+      />
+      <AuthInput 
+        label="Password" 
+        placeholder="6 characters" 
+        icon={lock} 
+        type={'password'}
+        defaultText={password}
+        handleChange={passwordHandler}
+        />
+      <AuthInput 
+        label="Confirm Password" 
+        placeholder="6 characters" 
+        icon={lock} 
+        type={'password'} 
+        defaultText={confirmedPassword}
+        handleChange={confirmedPasswordHandler}
+      />
 
       <div className={styles['bottom-actions']}>
-        <button type="submit" className={['reset-btn', styles['submit']].join(' ')}>Register</button>
+        <button 
+          type="submit" 
+          className={['reset-btn', styles['submit']].join(' ')}
+        >
+          Register
+        </button>
         <span className={styles['bottom-mss']}>
           Already have an account?
           <span 
